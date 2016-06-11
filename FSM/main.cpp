cf7cdd8c1a758fb50b7539b02f9b7d0d3e4e1535
Ivan Pankratov <ivan-pankratov2010@yandex.ru>
2016-06-11 16:45:32 +0300

    Testing code in main.cpp
    Добавил тестирующий код в main.cpp.
    
    Signed-off-by: Ivan Pankratov <ivan-pankratov2010@yandex.ru>

--- FSM/main.cpp
+++ FSM/main.cpp
@@ -5,5 +5,23 @@
 int main(int argc, char *argv[]) {
 	QCoreApplication a(argc, argv);
 
+	SPattern pat1;
+	pat1.data << 0x7;
+	pat1.mask << 0x7;
+	pat1.nLength = 3;
+	pat1.nMaxErrors = 1;
+
+	SPattern pat2;
+	pat2.data << 0x5;
+	pat2.mask << 0x7;
+	pat2.nLength = 3;
+	pat2.nMaxErrors = 1;
+
+	TPatterns patterns;
+	patterns << pat1 << pat2;
+
+	CFsmCreator fsm(patterns);
+	fsm.GenerateTables();
+
 	return 0;
 }
