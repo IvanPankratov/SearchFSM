42598921cad2c2075e22af9122eff4d7e56d98e6
Ivan Pankratov <ivan-pankratov2010@yandex.ru>
2016-12-30 20:35:00 +0300

    fsm-test: output table, refactoring
    Добавил в тестирующий класс автомата сохранение таблицы выходов и её использование при тестовом
    прогоне автомата.
    Добавил typedef для нескольких типов.
    
    Signed-off-by: Ivan Pankratov <ivan-pankratov2010@yandex.ru>

--- SearchFSM/FsmTest.cpp
+++ SearchFSM/FsmTest.cpp
@@ -8,18 +8,20 @@ unsigned char RandomByte() {
 
 CFsmTest::SWrapFsm CFsmTest::CreateFsm(const CFsmCreator &fsmCreator) {
 	// convert tables to format acceptable by CSearchFsm
-	QVector<TSearchFsm::STableRow> rows;
-	QVector<TSearchFsm::SOutput> outputs;
+	TFsmTable rows;
+	TOutputTable outputs;
 	int nRow;
 	for (nRow = 0; nRow < fsmCreator.GetStatesCount(); nRow++) {
 		STableRow row = fsmCreator.GetTableRow(nRow);
 		TSearchFsm::STableCell cell0;
 		cell0.idxNextState = row.cell0.nNextState;
-		cell0.idxOutput = TSearchFsm::sm_outputNull; // no output yet
+		TOutputIdx idxOutput0 = StoreOutputList(row.cell0.output, &outputs);
+		cell0.idxOutput = idxOutput0;
 
 		TSearchFsm::STableCell cell1;
 		cell1.idxNextState = row.cell1.nNextState;
-		cell1.idxOutput = TSearchFsm::sm_outputNull; // no output yet
+		TOutputIdx idxOutput1 = StoreOutputList(row.cell1.output, &outputs);
+		cell1.idxOutput = idxOutput1;
 
 		TSearchFsm::STableRow fsmRow = {cell0, cell1};
 		rows.append(fsmRow);
@@ -34,18 +36,64 @@ CFsmTest::SWrapFsm CFsmTest::CreateFsm(const CFsmCreator &fsmCreator) {
 
 bool CFsmTest::TestFsm(TSearchFsm fsm) {
 	fsm.Reset();
-	int nDataLength = 30;
+	int nDataLength = 50;
 	int idx;
 	for (idx = 0; idx < nDataLength; idx++) {
 		int nState = fsm.GetState();
 		unsigned char bBit = RandomByte() & 0x01;
 		int nOut = fsm.PushBit(bBit);
-		printf("%i =%i=> %i\n", nState, bBit, fsm.GetState());
-		while (nOut != TSearchFsm::sm_outputNull) {
-			// stub for output processing
-			break;
+		printf("%i =%i=> %i", nState, bBit, fsm.GetState());
+		if (nOut != TSearchFsm::sm_outputNull) {
+			printf(" {");
+			const TSearchFsm::SOutput &out = fsm.GetOutput(nOut);
+			printf("(%i, %i, %i)", out.bPatternIdx, out.bErrors, out.bPosition);
+			nOut = out.idxNextOutput;
+			while (nOut != TSearchFsm::sm_outputNull) {
+				const TSearchFsm::SOutput &out = fsm.GetOutput(nOut);
+				printf(", (%i, %i, %i)", out.bPatternIdx, out.bErrors, out.bPosition);
+				nOut = out.idxNextOutput;
+			}
+			printf("}");
 		}
+		printf("\n");
 	}
 
 	return true;
+}
+
+CFsmTest::TOutputIdx CFsmTest::StoreOutputList(const TOutputList &outputList, CFsmTest::TOutputTable *pOutputTable) {
+	if (outputList.isEmpty()) {
+		return TSearchFsm::sm_outputNull;
+	}
+
+	int idx;
+	TOutputIdx idxNext = TSearchFsm::sm_outputNull;
+	for (idx = outputList.count() - 1; idx >= 0; idx--) {
+		SOutput out = outputList[idx];
+		TSearchFsm::SOutput outputNew;
+		outputNew.bPatternIdx = out.nPatternIdx;
+		outputNew.bErrors = out.nErrors;
+		outputNew.bPosition = out.nPosition;
+		outputNew.idxNextOutput = idxNext;
+		idxNext = StoreOutput(outputNew, pOutputTable);
+	}
+
+	return idxNext;
+}
+
+CFsmTest::TOutputIdx CFsmTest::StoreOutput(const TSearchFsm::SOutput &output, CFsmTest::TOutputTable *pOutputTable) {
+	int idx;
+	for (idx = 0; idx < pOutputTable->count(); idx++) {
+		if (IsEqual(output, pOutputTable->at(idx))) {
+			return idx;
+		}
+	}
+
+	pOutputTable->push_back(output);
+	return pOutputTable->count() - 1;
+}
+
+bool CFsmTest::IsEqual(const TSearchFsm::SOutput &output1, const TSearchFsm::SOutput &output2) {
+	return (output1.bPatternIdx == output2.bPatternIdx) && (output1.bErrors == output2.bErrors) &&
+		(output1.bPosition == output2.bPosition) && (output1.idxNextOutput == output2.idxNextOutput);
 }
--- SearchFSM/FsmTest.h
+++ SearchFSM/FsmTest.h
@@ -8,16 +8,26 @@
 
 class CFsmTest {
 public:
-	typedef CSearchFsm<unsigned int, unsigned int> TSearchFsm;
+	typedef unsigned int TStateIdx;
+	typedef unsigned int TOutputIdx;
+	typedef CSearchFsm<TStateIdx, TOutputIdx> TSearchFsm;
+
+	typedef QVector<TSearchFsm::STableRow> TFsmTable;
+	typedef QVector<TSearchFsm::SOutput> TOutputTable;
 	struct SWrapFsm {
 		TSearchFsm fsm; // uses \var rows and \var output fields, do NOT use this field separately
-		QVector<TSearchFsm::STableRow> rows;
-		QVector<TSearchFsm::SOutput> outputs;
+		TFsmTable rows;
+		TOutputTable outputs;
 	};
 
 public:
 	static SWrapFsm CreateFsm(const CFsmCreator &fsmCreator);
 	static bool TestFsm(TSearchFsm fsm);
+
+private:
+	static TOutputIdx StoreOutputList(const TOutputList &outputList, /* in-out */ TOutputTable *pOutputTable);
+	static TOutputIdx StoreOutput(const TSearchFsm::SOutput &output, /* in-out */ TOutputTable *pOutputTable);
+	static bool IsEqual(const TSearchFsm::SOutput &output1, const TSearchFsm::SOutput &output2);
 };
 
 #endif // FSMTEST_H
