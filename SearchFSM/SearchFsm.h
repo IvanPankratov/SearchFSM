3d09f74285f885872cb7287144e5f60398399f86
Ivan Pankratov <ivan-pankratov2010@ya.ru>
2017-02-09 11:26:20 +0300

    CSearchFsmByte - template class for byte SearchFSM
    Написал шаблонный класс CSearchFsmByte для реализации байтового поискового автомата.
    
    Signed-off-by: Ivan Pankratov <ivan-pankratov2010@ya.ru>

--- SearchFSM/SearchFsm.h
+++ SearchFSM/SearchFsm.h
@@ -9,7 +9,7 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-/// \brief CSearchFsm<TStateIdx, TOutputIdx> class - template for SearchFSM
+/// \brief CSearchFsm<TStateIdx, TOutputIdx, TPatternIdx, TStepBack, TErrorsCount> class - template for SearchFSM
 template <class TStateIdx = unsigned int, class TOutputIdx = unsigned int, class TPatternIdx = unsigned int,
 	class TStepBack = unsigned int, class TErrorsCount = unsigned int>
 class CSearchFsm {
@@ -64,6 +64,68 @@ public: // working methods
 	}
 
 	const SOutput& GetOutput(TOutputIdx idxOutput) const {
+		ASSERT(idxOutput<m_table.outputsCount);
+		return m_table.pOutputs[idxOutput];
+	}
+
+private:
+	const STable m_table;
+	TStateIdx m_state;
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+/// \brief CSearchFsmByte<nBitsAtOnce, TStateIdx, TOutputIdx, TPatternIdx, TStepBack, TErrorsCount>
+/// template class for byte SearchFSM
+template <const int nBitsAtOnce, class TStateIdx = unsigned int, class TOutputIdx = unsigned int,
+	class TPatternIdx = unsigned int, 	class TStepBack = unsigned int, class TErrorsCount = unsigned int>
+class CSearchFsmByte {
+public:
+	static const TOutputIdx sm_outputNull = (TOutputIdx)(-1);
+	static const int g_nColumnsCount = (1 << nBitsAtOnce);
+	static const unsigned int g_dwByteMask = g_nColumnsCount - 1;
+
+	// alias to the similar CSearchFsm and its types
+	typedef CSearchFsm<TStateIdx, TOutputIdx, TPatternIdx, TStepBack, TErrorsCount> TSearchFsm;
+	typedef typename TSearchFsm::STableCell TTableCell;
+	typedef typename TSearchFsm::SOutput TOutput;
+
+	// FSM table structures
+	struct STableRow {
+		TTableCell cells[g_nColumnsCount];
+	};
+
+	// Whole automaton table structure
+	struct STable {
+		const STableRow *pTableRows;
+		const TOutput *pOutputs;
+		TStateIdx statesCount;
+		TOutputIdx outputsCount;
+	};
+
+public: // constructor
+	CSearchFsmByte(const STable &table): m_table(table) {
+		Reset();
+	}
+
+public: // working methods
+	void Reset(TStateIdx state = 0) {
+		ASSERT(state < m_table.statesCount);
+		m_state = state;
+	}
+
+	TOutputIdx PushByte(unsigned int dwValue) {
+		const STableRow &row = m_table.pTableRows[m_state];
+		const TTableCell &cell = row.cells[dwValue & g_dwByteMask];
+		m_state = cell.idxNextState;
+		return cell.idxOutput;
+	}
+
+	TStateIdx GetState() const {
+		return m_state;
+	}
+
+	const typename TSearchFsm::SOutput& GetOutput(TOutputIdx idxOutput) const {
 		ASSERT(idxOutput<m_table.outputsCount);
 		return m_table.pOutputs[idxOutput];
 	}
